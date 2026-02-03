9def97fb4aef5fc0c619605d6bb3f6acb626d410
galichetethan37-beep <galichetethan37@gmail.com>
2026-02-03 13:45:59 +0100

    Add winter camp booking page with push notifications

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("push", (event) => {
+  if (!event.data) {
+    return;
+  }
+
+  const data = event.data.json();
+  const options = {
+    body: data.body,
+    icon: "/icons/icon-192.svg",
+    badge: "/icons/icon-192.svg",
+    data: data.url || "/",
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data));
+});
